b09167ea2c5a938831ee5b9956770939d07c5a66
DaMemes2 <yachenyao123@gmail.com>
2026-06-22 00:58:37 +0800

    Add files via upload

---
+++ levels/wf/textureNew.inc.c
@@ -0,0 +1,369 @@
+ALIGNED8 u8 wf_1__texture_0E000018[] = {
+#include "levels/wf/wf_1_0xe000018_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E001020[] = {
+#include "levels/wf/wf_1_0xe001020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E002020[] = {
+#include "levels/wf/wf_1_0xe002020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E002820[] = {
+#include "levels/wf/wf_1_0xe002820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E003020[] = {
+#include "levels/wf/wf_1_0xe003020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E003820[] = {
+#include "levels/wf/wf_1_0xe003820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E004020[] = {
+#include "levels/wf/wf_1_0xe004020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E005820[] = {
+#include "levels/wf/wf_1_0xe005820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E006820[] = {
+#include "levels/wf/wf_1_0xe006820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E007820[] = {
+#include "levels/wf/wf_1_0xe007820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E008020[] = {
+#include "levels/wf/wf_1_0xe008020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E009820[] = {
+#include "levels/wf/wf_1_0xe009820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E00A020[] = {
+#include "levels/wf/wf_1_0xe00a020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E00A820[] = {
+#include "levels/wf/wf_1_0xe00a820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E00B820[] = {
+#include "levels/wf/wf_1_0xe00b820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E00C020[] = {
+#include "levels/wf/wf_1_0xe00c020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E00C820[] = {
+#include "levels/wf/wf_1_0xe00c820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E00D020[] = {
+#include "levels/wf/wf_1_0xe00d020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E059820[] = {
+#include "levels/wf/wf_1_0xe059820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E05A260[] = {
+#include "levels/wf/wf_1_0xe05a260_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E05AC40[] = {
+#include "levels/wf/wf_1_0xe05ac40_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E00E820[] = {
+#include "levels/wf/wf_1_0xe00e820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E00F020[] = {
+#include "levels/wf/wf_1_0xe00f020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E00F820[] = {
+#include "levels/wf/wf_1_0xe00f820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E010020[] = {
+#include "levels/wf/wf_1_0xe010020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E011020[] = {
+#include "levels/wf/wf_1_0xe011020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E012020[] = {
+#include "levels/wf/wf_1_0xe012020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E012820[] = {
+#include "levels/wf/wf_1_0xe012820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E013020[] = {
+#include "levels/wf/wf_1_0xe013020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E014020[] = {
+#include "levels/wf/wf_1_0xe014020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E015020[] = {
+#include "levels/wf/wf_1_0xe015020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E016020[] = {
+#include "levels/wf/wf_1_0xe016020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E017020[] = {
+#include "levels/wf/wf_1_0xe017020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E018020[] = {
+#include "levels/wf/wf_1_0xe018020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E019020[] = {
+#include "levels/wf/wf_1_0xe019020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E01A020[] = {
+#include "levels/wf/wf_1_0xe01a020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E01B020[] = {
+#include "levels/wf/wf_1_0xe01b020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E01B820[] = {
+#include "levels/wf/wf_1_0xe01b820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E01C820[] = {
+#include "levels/wf/wf_1_0xe01c820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E01D820[] = {
+#include "levels/wf/wf_1_0xe01d820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E01E020[] = {
+#include "levels/wf/wf_1_0xe01e020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E01F020[] = {
+#include "levels/wf/wf_1_0xe01f020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E020020[] = {
+#include "levels/wf/wf_1_0xe020020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E020820[] = {
+#include "levels/wf/wf_1_0xe020820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E021020[] = {
+#include "levels/wf/wf_1_0xe021020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E022820[] = {
+#include "levels/wf/wf_1_0xe022820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E025020[] = {
+#include "levels/wf/wf_1_0xe025020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E026020[] = {
+#include "levels/wf/wf_1_0xe026020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E027020[] = {
+#include "levels/wf/wf_1_0xe027020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E027820[] = {
+#include "levels/wf/wf_1_0xe027820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E028820[] = {
+#include "levels/wf/wf_1_0xe028820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E029820[] = {
+#include "levels/wf/wf_1_0xe029820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E02A820[] = {
+#include "levels/wf/wf_1_0xe02a820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E02B820[] = {
+#include "levels/wf/wf_1_0xe02b820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E02C020[] = {
+#include "levels/wf/wf_1_0xe02c020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E02C820[] = {
+#include "levels/wf/wf_1_0xe02c820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E02D020[] = {
+#include "levels/wf/wf_1_0xe02d020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E02D820[] = {
+#include "levels/wf/wf_1_0xe02d820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E02E020[] = {
+#include "levels/wf/wf_1_0xe02e020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E02E820[] = {
+#include "levels/wf/wf_1_0xe02e820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E02F820[] = {
+#include "levels/wf/wf_1_0xe02f820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E030020[] = {
+#include "levels/wf/wf_1_0xe030020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E030820[] = {
+#include "levels/wf/wf_1_0xe030820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E031820[] = {
+#include "levels/wf/wf_1_0xe031820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E032020[] = {
+#include "levels/wf/wf_1_0xe032020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E032820[] = {
+#include "levels/wf/wf_1_0xe032820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E033020[] = {
+#include "levels/wf/wf_1_0xe033020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E033820[] = {
+#include "levels/wf/wf_1_0xe033820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E034820[] = {
+#include "levels/wf/wf_1_0xe034820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E035020[] = {
+#include "levels/wf/wf_1_0xe035020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E036020[] = {
+#include "levels/wf/wf_1_0xe036020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E036820[] = {
+#include "levels/wf/wf_1_0xe036820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E037820[] = {
+#include "levels/wf/wf_1_0xe037820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E038020[] = {
+#include "levels/wf/wf_1_0xe038020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E038820[] = {
+#include "levels/wf/wf_1_0xe038820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E039020[] = {
+#include "levels/wf/wf_1_0xe039020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E039820[] = {
+#include "levels/wf/wf_1_0xe039820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E03D020[] = {
+#include "levels/wf/wf_1_0xe03d020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E040020[] = {
+#include "levels/wf/wf_1_0xe040020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E040820[] = {
+#include "levels/wf/wf_1_0xe040820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E041020[] = {
+#include "levels/wf/wf_1_0xe041020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E041820[] = {
+#include "levels/wf/wf_1_0xe041820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E042020[] = {
+#include "levels/wf/wf_1_0xe042020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E043820[] = {
+#include "levels/wf/wf_1_0xe043820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E044820[] = {
+#include "levels/wf/wf_1_0xe044820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E045820[] = {
+#include "levels/wf/wf_1_0xe045820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E046820[] = {
+#include "levels/wf/wf_1_0xe046820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E047820[] = {
+#include "levels/wf/wf_1_0xe047820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E048820[] = {
+#include "levels/wf/wf_1_0xe048820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E049820[] = {
+#include "levels/wf/wf_1_0xe049820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E04A820[] = {
+#include "levels/wf/wf_1_0xe04a820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E04B820[] = {
+#include "levels/wf/wf_1_0xe04b820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E04C820[] = {
+#include "levels/wf/wf_1_0xe04c820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E04D820[] = {
+#include "levels/wf/wf_1_0xe04d820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E04E820[] = {
+#include "levels/wf/wf_1_0xe04e820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E04F820[] = {
+#include "levels/wf/wf_1_0xe04f820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E050820[] = {
+#include "levels/wf/wf_1_0xe050820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E051820[] = {
+#include "levels/wf/wf_1_0xe051820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E052820[] = {
+#include "levels/wf/wf_1_0xe052820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E053820[] = {
+#include "levels/wf/wf_1_0xe053820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E054820[] = {
+#include "levels/wf/wf_1_0xe054820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E055820[] = {
+#include "levels/wf/wf_1_0xe055820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E056820[] = {
+#include "levels/wf/wf_1_0xe056820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E005020[] = {
+#include "levels/wf/wf_1_0xe005020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E009020[] = {
+#include "levels/wf/wf_1_0xe009020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E00E020[] = {
+#include "levels/wf/wf_1_0xe00e020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E022020[] = {
+#include "levels/wf/wf_1_0xe022020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E023020[] = {
+#include "levels/wf/wf_1_0xe023020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E024020[] = {
+#include "levels/wf/wf_1_0xe024020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E024820[] = {
+#include "levels/wf/wf_1_0xe024820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E028020[] = {
+#include "levels/wf/wf_1_0xe028020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E03A820[] = {
+#include "levels/wf/wf_1_0xe03a820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E03B020[] = {
+#include "levels/wf/wf_1_0xe03b020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E03E020[] = {
+#include "levels/wf/wf_1_0xe03e020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E03F020[] = {
+#include "levels/wf/wf_1_0xe03f020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E042820[] = {
+#include "levels/wf/wf_1_0xe042820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E057820[] = {
+#include "levels/wf/wf_1_0xe057820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E058020[] = {
+#include "levels/wf/wf_1_0xe058020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E058820[] = {
+#include "levels/wf/wf_1_0xe058820_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E059020[] = {
+#include "levels/wf/wf_1_0xe059020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E03A020[] = {
+#include "levels/wf/wf_1_0xe03a020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E03C020[] = {
+#include "levels/wf/wf_1_0xe03c020_custom.rgba16.inc.c"
+};
+ALIGNED8 u8 wf_1__texture_0E043020[] = {
+#include "levels/wf/wf_1_0xe043020_custom.rgba16.inc.c"
+};

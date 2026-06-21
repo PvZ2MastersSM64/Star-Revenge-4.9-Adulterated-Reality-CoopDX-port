056a6279b33e45d41ddb6d2f0b135a529ac169b2
Rogergm1 <rogergalindo192@gmail.com>
2026-06-22 00:03:28 +0200

    many actors :3

---
+++ actors/bubble/geo.inc.c
@@ -0,0 +1,9 @@
+#include "model.inc.h"
+
+const GeoLayout purple_marble_geo[]= {
+	GEO_SWITCH_CASE(1,  geo_switch_anim_state),
+	GEO_OPEN_NODE(),
+		GEO_DISPLAY_LIST(4, DL_purple_marble_geo_0x401dde0),
+	GEO_CLOSE_NODE(),
+	GEO_END(),
+};
